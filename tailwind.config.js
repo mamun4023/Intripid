47fe9e4074102c5f371c12039f2a0f17fcf79d5e
mamun4023 <mamun754023@gmail.com>
2023-06-10 12:20:30 +0600

    project setup has been completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+   content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+   ],
+   theme: {
+      screens: {
+         mobile: 360,
+         tablet: 768,
+         laptop: 1280,
+         desktop: 1440,
+         lageDesktop: 2560,
       },
-    },
-  },
-  plugins: [],
-}
+      extend: {
+         fontFamily: {
+            readex_600: ["readex-600"],
+            sans_400: ["jakarta_sans-400"],
+            sans_500: ["jakarta_sans-500"],
+         },
+      },
+      colors: {
+         primary: "#F47F22",
+         white: "#fff",
+         dark: "#31374A",
+      },
+   },
+   plugins: [],
+};
